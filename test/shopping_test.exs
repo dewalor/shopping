89acb615c2f966a1e0806d14824c8d42bb3e8ab3
dewalor <dewalor@proton.me>
2025-02-08 20:59:06 +0800

    test refactor

--- test/shopping_test.exs
+++ test/shopping_test.exs
@@ -1,4 +1,4 @@
-defmodule Shopping0Test do
+defmodule ShoppingTest do
   use ExUnit.Case, async: true
   doctest Shopping
 
@@ -30,14 +30,6 @@ defmodule Shopping0Test do
     assert Shopping.check_out("CF1") == 1123
   end
 
-  test "gives the right price for a basket with all three items, no discount" do
-    assert Shopping.check_out("GR1,SR1,CF1") == 1934
-  end
-
-  test "gives the right price for a basket with three green teas and other items" do
-    assert Shopping.check_out("GR1,SR1,GR1,GR1,CF1") == 2245
-  end
-
   test "gives the right price for a basket with two green teas" do
     assert Shopping.check_out("GR1,GR1") == 311
   end
@@ -50,23 +42,51 @@ defmodule Shopping0Test do
     assert Shopping.check_out("CF1,CF1") == 2246
   end
 
+  test "gives the right price for a basket with all three items, no discount" do
+    basket = "GR1,SR1,CF1"
+    assert Shopping.check_out(basket) == 1934
+    assert Shopping.check_out(reorder(basket)) == 1934
+  end
+
+  test "gives the right price for a basket with three green teas and other items" do
+    basket = "GR1,SR1,GR1,GR1,CF1"
+    assert Shopping.check_out(basket) == 2245
+    assert Shopping.check_out(reorder(basket)) == 2245
+  end
+
   test "gives the right price for a basket with three strawberries and other items" do
-    assert Shopping.check_out("SR1,SR1,GR1,SR1") == 1661
+    basket = "SR1,SR1,GR1,SR1"
+    assert Shopping.check_out(basket) == 1661
+    assert Shopping.check_out(reorder(basket)) == 1661
   end
 
   test "gives the right price for a basket with four strawberries and other items" do
-    assert Shopping.check_out("SR1,SR1,GR1,SR1,SR1") == 2111
+    basket = "SR1,SR1,GR1,SR1,SR1"
+    assert Shopping.check_out(basket) == 2111
+    assert Shopping.check_out(reorder(basket)) == 2111
   end
 
   test "gives the right price for a basket with three coffees and other items" do
-    assert Shopping.check_out("GR1,CF1,SR1,CF1,CF1") == 3057
+    basket = "GR1,CF1,SR1,CF1,CF1"
+    assert Shopping.check_out(basket) == 3057
+    assert Shopping.check_out(reorder(basket)) == 3057
   end
 
   test "gives the right price for a basket with four coffees and other items" do
-    assert Shopping.check_out("GR1,CF1,SR1,CF1,CF1,CF1") == 3806
+    basket = "GR1,CF1,SR1,CF1,CF1,CF1"
+    assert Shopping.check_out(basket) == 3806
+    assert Shopping.check_out(reorder(basket)) == 3806
   end
 
   test "gives the right price for a basket with all three discounts" do
-    assert Shopping.check_out("SR1,SR1,GR1,SR1,GR1,CF1,CF1,CF1") == 3907
+    basket = "SR1,SR1,GR1,SR1,GR1,CF1,CF1,CF1"
+    assert Shopping.check_out(basket) == 3907
+    assert Shopping.check_out(reorder(basket)) == 3907
+  end
+
+  defp reorder(items) when is_binary items do
+    String.split(items, ",", trim: true)
+              |> Enum.shuffle()
+              |> Enum.join(",")
   end
 end
