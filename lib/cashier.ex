26ee4d094f75ccf57af0e7eee177ea5c49cbe683
dewalor <dewalor@proton.me>
2025-02-09 11:13:54 +0800

    test for genserver termination

--- lib/cashier.ex
+++ lib/cashier.ex
@@ -1,5 +1,6 @@
 defmodule Cashier do
   use GenServer
+  require Logger
   # Client
 
   def start_link(basket) when is_list basket do
@@ -15,12 +16,11 @@ defmodule Cashier do
   end
 
   # Server (callbacks)
-
   @impl true
   def init(basket) when is_list basket do
-    # the first element is a map of products in the basket that have been processed/checked out
-    # the second element is the list of products in the basket that haven't been checked out, i.e. unprocessed
-    initial_state = {%{}, basket}
+    Process.flag(:trap_exit, true)
+
+    initial_state = {%{}, basket} # {processed/checked out, unprocessed}
     {:ok, initial_state}
   end
 
@@ -34,23 +34,15 @@ defmodule Cashier do
 
   @impl true
   def handle_call(:total, from, {processed, [product | tail] = _unprocessed}) do
-    # processed e.g. %{GR1: {1, 311}}, i.e. %{product: {total quantity, total price}}
-
+    # processed = %{product: {total quantity, total price}}
     processed = Map.update(
                   processed,
                   product,
                   {1, Shopping.get_price!(product)},
                   fn {qty, total_price} -> {qty + 1, Shopping.calculate_total_price(product, qty + 1, total_price)} end
                 )
-    # the first element is the new map of checked out/processed products
-    # the second element is the list of remaining unprocessed products to be checked out recursively
-    handle_call(:total, from, {processed, tail})
-  end
 
-  # 3 or more coffees, the price of all coffees should drop to two thirds of the original price; subtract 1/3 price per coffee
-  @impl true
-  def handle_call(:subtract, _from, {total, discount, unprocessed_items}) do
-    {:reply, discount, {total, unprocessed_items}}
+                handle_call(:total, from, {processed, tail})
   end
 
   @impl true
@@ -59,8 +51,13 @@ defmodule Cashier do
   end
 
   @impl true
-  def terminate(_reason, state) do
-    #Logger.info "terminating #{reason}"
+  def handle_info({:kill, _from, reason}, state) do
+    {:stop, reason, state}
+  end
+
+  @impl true
+  def terminate(reason, state) do
+    Logger.info "terminating #{reason}"
     state
   end
 end
--- test/cashier_test.exs
+++ test/cashier_test.exs
@@ -5,4 +5,18 @@ defmodule CashierTest do
     {:ok, pid} = CashierSupervisor.start_child({Cashier, [:GR1]})
     assert GenServer.call(pid, :total) == 311
   end
+
+  test "throws match error when given an invalid params" do
+    assert_raise MatchError, fn ->
+      {:ok, _} = CashierSupervisor.start_child({Cashier, "GR1"})
+    end
+  end
+
+  test "terminates with :ok" do
+    {:ok, pid} = CashierSupervisor.start_child({Cashier, [:GR1]})
+    GenServer.call(pid, :total)
+
+    assert :ok =  GenServer.stop(pid)
+
+  end
 end
