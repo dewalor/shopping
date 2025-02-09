9b608a08add777d39725c146099ad3fe04963a0f
dewalor <dewalor@proton.me>
2025-02-09 17:48:42 +0800

    agent and cashier tests

--- test/cashier_test.exs
+++ test/cashier_test.exs
@@ -2,21 +2,23 @@ defmodule CashierTest do
   use ExUnit.Case, async: true
 
   test "gives the right total when given a valid product list" do
-    {:ok, pid} = CashierSupervisor.start_child({Cashier, [:GR1]})
+    {:ok, state_pid_0} = CashierStateSupervisor.start_child({CashierState, name: String.to_atom("Supervisor1")})
+    {:ok, pid} = CashierSupervisor.start_child({Cashier, [[:GR1], state_pid_0]})
     assert GenServer.call(pid, :total) == 311
   end
 
-  test "throws match error when given an invalid params" do
-    assert_raise MatchError, fn ->
-      {:ok, _} = CashierSupervisor.start_child({Cashier, "GR1"})
-    end
+  test "throws function clause error when given an invalid params" do
+      {:ok, state_pid_1} = CashierStateSupervisor.start_child({CashierState, name: String.to_atom("Supervisor2")})
+      assert_raise MatchError, fn ->
+        {:ok, _} = CashierSupervisor.start_child({Cashier, ["GR1", state_pid_1]})
+      end
   end
 
   test "terminates with :ok" do
-    {:ok, pid} = CashierSupervisor.start_child({Cashier, [:GR1]})
+    {:ok, state_pid_2} = CashierStateSupervisor.start_child({CashierState, name: String.to_atom("Supervisor3")})
+    {:ok, pid} = CashierSupervisor.start_child({Cashier, [[:GR1], state_pid_2]})
     GenServer.call(pid, :total)
 
     assert :ok =  GenServer.stop(pid)
-
   end
 end
