a637238583deca11d93bc0e01dd864e5ac7c1ec6
dewalor <dewalor@proton.me>
2025-02-17 16:42:22 +0800

    refactor view_total

--- lib/shopping.ex
+++ lib/shopping.ex
@@ -31,11 +31,7 @@ defmodule Shopping do
     basket_id = generate_basket_id()
     Dispatcher.scan_basket(%{items: items, basket_id: basket_id})
 
-    {:ok, total} = Dispatcher.get_total(basket_id)
-    case total do
-      x when is_integer(x) -> to_GBP_string(x)
-      error -> error
-    end
+    view_total(basket_id)
   end
 
   def view_total(basket_id) when Kernel.is_binary(basket_id) do
